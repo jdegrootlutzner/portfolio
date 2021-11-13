ec58f338a7f8f99c6c035fd06d4d23d3761b89b3
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-11-13 00:08:21 -0500

    Attempt to fix deployment and also adding Tailwind purge
    - remove img with empty src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ["*.html", "*/*.html"],
+    safelist: ['btn',]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
