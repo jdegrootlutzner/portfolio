43f080c29416069902e463f6aba2eea679f3de67
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-12-02 15:36:36 -0500

    Attempt to fix build
    - add content to each page
    - fix links
    - change config

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,16 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        pentridge: resolve(__dirname, 'pentridge-station/index.html'),
-        privacy: resolve(__dirname, 'digital-privacy/index.html'),
+        pentridge: resolve(__dirname, 'pentridge/index.html'),
+        privacy: resolve(__dirname, 'digital/index.html'),
         honeybis: resolve(__dirname, 'honeybis/index.html'),
         redial: resolve(__dirname, 'redial/index.html'),
         citibikes: resolve(__dirname, 'citbikes/index.html'),
         chiphoagie: resolve(__dirname, 'chiphoagie/index.html'),
-        chinook: resolve(__dirname, 'chinook-fund/index.html'),
-        tables: resolve(__dirname, 'bedside-tables/index.html'),
+        chinook: resolve(__dirname, 'chinook/index.html'),
+        tables: resolve(__dirname, 'tables/index.html'),
         redial: resolve(__dirname, 'redial/index.html'),
-        cloud: resolve(__dirname, 'cloud-under-the-sea/index.html'),
+        cloud: resolve(__dirname, 'cloud/index.html'),
 
 
       }
